849d058a60110bd4f5ed23b12f8a5b78f19d14fc
rafael <bboyrafinhazika@gmail.com>
2023-05-19 21:23:24 -0300

    upload project

---
+++ dell.py
@@ -0,0 +1,127 @@
+from selenium import webdriver
+
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.common.by import By
+import time
+
+SLEEP_TIME = 4
+
+class DellBot():
+    def __init__(self) -> None:
+        self.retailer_id = 'b43b67ad-a6f4-4525-8a8e-31a829e81c39'
+        self.cashbackProviders = [{
+                "name": "Cuponomia",
+                "affiliatedLink": "https://www.cuponomia.com.br/ref/a8a8ec1cba89",
+                "url": "https://www.cuponomia.com.br/desconto/dell",
+                "xpath": "/html/body/section[1]/div[1]/div[1]/div/aside/a/span",
+            }, {
+                "name": "Meliuz",
+                "affiliatedLink": "https://www.meliuz.com.br/i/ref_bae7d6a1?ref_source=2",
+                "url": "https://www.meliuz.com.br/desconto/cupom-dell",
+                "xpath": "/html/body/div[3]/div[4]/button",
+            }, 
+        ]
+        op = webdriver.ChromeOptions()
+        op.add_argument("--window-size=1920,1080")
+        op.add_argument('--disable-gpu')
+        op.add_argument('--no-sandbox')
+        #op.add_argument('--headless')
+        op.add_argument('--disable-dev-shm-usage')
+        op.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42")
+        service = Service(ChromeDriverManager().install())
+        #op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        self.browser = webdriver.Chrome(service=service, options=op)
+    
+    
+    def accessCart(self, url: str) -> int:
+        self.browser.get(url)
+        time.sleep(SLEEP_TIME)
+        try:
+            price = int(self.browser.find_element(By.XPATH, '/html/body/main/section[2]/div[1]/div[1]/div[2]/div[2]/span[1]/span[2]').text[3:].replace(',', '').replace('.', ''))
+        except Exception as e:
+            print(e)
+            raise
+        try:
+            self.browser.find_element(By.XPATH, '//*[@id="add-to-cart-stack"]/div[2]/button').click()
+        except Exception as e:
+            print(e)
+        time.sleep(SLEEP_TIME)
+        self.browser.get('https://www.dell.com/pt-br/cart')
+        time.sleep(SLEEP_TIME)
+        
+        return price
+    
+    def tryCoupon(self, couponCode: str, lastPrice:int, **kwargs) -> int:
+        currentPrice = lastPrice
+        priceReturn = 0
+        try:
+            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/input').clear()
+            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/input').send_keys(couponCode)
+            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/span/button').click()
+            time.sleep(SLEEP_TIME)
+            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/span/button').click()
+            time.sleep(SLEEP_TIME)
+            currentPrice = int(self.browser.find_element(By.XPATH, '//*[@id="nonpcaas-cart-summary"]/div[3]/ul/li/div[2]/strong').text[2:].replace(',', '').replace('.', '')) 
+        except:
+            time.sleep(30)
+            print('Retrying coupon')
+            priceReturn = self.tryCoupon(couponCode, lastPrice)
+        if currentPrice < lastPrice:
+            priceReturn = currentPrice
+        return priceReturn
+    
+    def removeFromCart(self):
+        try:
+            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[2]/section/div[2]/div/div[2]/div/div/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a').click()
+            time.sleep(SLEEP_TIME)
+        except:
+            time.sleep(30)
+            self.removeFromCart()
+        
+    def removeCoupon(self, couponRank, restarted=False):
+        try:
+            if couponRank == 1:
+                self.browser.find_element(By.XPATH, f'/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[1]/div[1]/div[2]/p[2]/small/a[2]').click()
+                time.sleep(SLEEP_TIME)
+                self.browser.find_element(By.XPATH, f'/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[3]/div/removeappliedcouponmodal/div[3]/button[2]').click()
+            else:
+                self.browser.find_element(By.XPATH, f'/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[1]/div[2]/div[2]/p[2]/small/a[2]').click()
+                time.sleep(SLEEP_TIME)
+                self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[5]/div/removeappliedcouponmodal/div[3]/button[2]').click()
+        except Exception as e:
+            print('Reiniciando processo de remover cupom.')
+            time.sleep(30)
+            if restarted: return
+            self.removeCoupon(couponRank, restarted=True)
+            
+        time.sleep(SLEEP_TIME)
+    
+    def closeBrowser(self):
+        self.browser.quit()
+        
+    def bestCashbackFinder(self):
+        cashback = {}
+        try:
+            for cashbackProvider in self.cashbackProviders:
+                self.browser.get(cashbackProvider['url'])
+                time.sleep(SLEEP_TIME)
+                cashbackFullLabelArray = self.browser.find_element(By.XPATH, cashbackProvider['xpath']).text.split(' ')
+                for label in cashbackFullLabelArray:
+                    if '%' in label:
+                        cashbackProvider['value'] = int(label[:label.find('%')])
+                        if not cashback or cashback['value'] < cashbackProvider['value']:
+                            cashback = cashbackProvider
+        except:
+            print('erro na busca de cashbacks')
+        return cashback
+    
+if __name__ == '__main__':
+    url = 'https://www.dell.com/pt-br/shop/notebooks-dell/notebook-inspiron-15/spd/inspiron-15-5510-laptop/i5510u4101w'
+    k = DellBot()
+    price = k.accessCart(url)
+    available, price = k.tryCoupon('BEMVINDO150', lastPrice=price)
+    print(available, price)
+    #k.tryCoupon('SEJANINJA')
+    #print(k.couponVerify(url, 'SEJANINJA'))
---
+++ main.py
@@ -0,0 +1,73 @@
+from dell import DellBot
+import api
+import time
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+import json
+
+def chooseBestCoupon(validCoupons: list, allCoupons: list) -> dict:
+    if not validCoupons:
+        return None, 0
+    bestCoupon = validCoupons[0]
+    for coupon in validCoupons:
+        if coupon['discount'] > bestCoupon['discount']:
+            bestCoupon = coupon
+    bestCouponDiscount = bestCoupon['discount']
+    bestCoupon['discount'] = bestCoupon['discountLabel']
+    bestCoupon.pop('discountLabel')
+   
+    couponFinded = False
+    for coupon in allCoupons:
+        if bestCoupon['code'] == coupon['code']:
+            bestCoupon['id'] = coupon['id']
+            couponFinded = True
+            break
+    if not couponFinded:
+        r = api.create_coupon(bestCoupon)
+        bestCoupon['id'] = json.loads(r.content)['id']
+        
+    return bestCoupon['id'], bestCouponDiscount
+
+def main():
+    bot = DellBot()
+    allCoupons = api.get_coupons(bot.retailer_id)
+    coupons = [coupon for coupon in allCoupons if coupon['available'] and ' + ' not in coupon['code']]
+    products = api.get_retailer_products(bot.retailer_id)
+    cashback = bot.bestCashbackFinder()
+    for product in products:
+        print('produto: ', product['title'], product['html_url'])
+        data = {}
+        data['price'] = bot.accessCart(product['html_url'])
+        data['available'] = True
+        validCoupons = []
+        for firstCoupon in coupons:
+            currentPrice = bot.tryCoupon(firstCoupon['code'], data['price'])
+            if currentPrice:
+                validCoupons.append({"code": firstCoupon['code'], "discount": data['price'] - currentPrice, "available": True, "retailer_id": bot.retailer_id, "discountLabel": firstCoupon['discount'],
+                                     "comments": firstCoupon['comments']})
+                for secondCoupon in [coupon for coupon in coupons if coupon != firstCoupon]:
+                    secondCurrentPrice = bot.tryCoupon(secondCoupon['code'], currentPrice)
+                    if secondCurrentPrice:
+                        validCoupons.append({"code": f'{firstCoupon["code"]} + {secondCoupon["code"]}', "discount": data['price'] - secondCurrentPrice, "available": True, "retailer_id": bot.retailer_id, "discountLabel": f'{firstCoupon["discount"]} + {secondCoupon["discount"]}',
+                                             "comments": "Combinação de cupons disponível na Dell. Obs: não funciona em todos os produtos."})
+                        bot.removeCoupon(2)
+                bot.removeCoupon(1)
+        bestCouponId, bestCouponDiscount = chooseBestCoupon(validCoupons, allCoupons)
+        cashbackPercent = 0
+        if cashback: cashbackPercent = cashback['value']
+        finalPrice = int((data['price'] - bestCouponDiscount)*(1-cashbackPercent/100))
+        if finalPrice != data['price'] - bestCouponDiscount:
+            r = api.update_product_retailers(product['id'], bot.retailer_id, {"price": finalPrice, "coupon_id": bestCouponId, "cashback": cashback})
+            if r.status_code == 200:
+                print(f'{product["title"]} -> atualizado com sucesso!')
+            else:
+                print(r.content)
+        bot.removeFromCart()
+  
+main()  
+# bot = DellBot()
+# allCoupons = api.get_coupons(bot.retailer_id)
+# validCoupons = [{"code": "CUPONOMIANB + BEMVINDO150", "discount": 40000, "available": True, "retailer_id": "bot.retailer_id", "discountLabel": "oi"}]
+# chooseBestCoupon(validCoupons, allCoupons)
+                
