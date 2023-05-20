78bedd8c05002d92c1bacced83bba18c13f7418e
rafael <bboyrafinhazika@gmail.com>
2023-05-19 23:05:45 -0300

    final commit

--- dell.py
+++ dell.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 import os
@@ -29,9 +29,9 @@ class DellBot():
         op.add_argument('--headless')
         op.add_argument('--disable-dev-shm-usage')
         op.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42")
-        #service = Service(ChromeDriverManager().install())
-        op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        self.browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=op)
+        service = Service(ChromeDriverManager().install())
+        #op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        self.browser = webdriver.Chrome(service=service, options=op)
     
     
     def accessCart(self, url: str) -> int:
