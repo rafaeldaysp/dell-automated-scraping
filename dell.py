a7ac3e9b06794124f1146fa6c8aa9f975ed26b15
rafael <bboyrafinhazika@gmail.com>
2023-07-25 19:20:10 -0300

    commit changes

--- dell.py
+++ dell.py
@@ -47,18 +47,40 @@ class DellBot():
     def accessCart(self, url: str) -> int:
         self.browser.get(url)
         time.sleep(SLEEP_TIME)
-        try:
-            price = int(self.browser.find_element(By.XPATH, '/html/body/main/section[2]/div[1]/div[1]/div[2]/div[2]/span[1]/span[2]').text[3:].replace(',', '').replace('.', ''))
-        except Exception as e:
+        
+        
+        price_xpath_list = [
+            '/html/body/main/section[2]/div[1]/div[1]/div[2]/div[2]/span[1]/span[2]',
+            '//*[@id="cf-body"]/div[4]/div[2]/div[2]/div/div[2]/div',
+            '/html/body/div[4]/div[1]/div[5]/div/div/article/section/div[2]/div[1]/div[1]',
+            '//*[@id="460-bczs"]/section/div[2]/div[1]/div[1]'
+        ]
+        element_located = False
+        price = 0
+        for xpath in price_xpath_list:
             try:
-                price = int(self.browser.find_element(By.XPATH, '//*[@id="cf-body"]/div[4]/div[2]/div[2]/div/div[2]/div').text[3:].replace(',', '').replace('.', ''))
+                price = int(self.browser.find_element(By.XPATH, xpath).text[3:].replace(',', '').replace('.', ''))
+                element_located = True
+                break
             except:
-                raise
+                pass
+        if not element_located:
+            raise
+        print(price)
+        # try:
+        #     price = int(self.browser.find_element(By.XPATH, '/html/body/main/section[2]/div[1]/div[1]/div[2]/div[2]/span[1]/span[2]').text[3:].replace(',', '').replace('.', ''))
+        # except Exception as e:
+        #     try:
+        #         price = int(self.browser.find_element(By.XPATH, '//*[@id="cf-body"]/div[4]/div[2]/div[2]/div/div[2]/div').text[3:].replace(',', '').replace('.', ''))
+        #     except:
+        #         raise
         button_xpath_list = [
             '//*[@id="add-to-cart-stack"]/div[2]/button',
             '//*[@id="cf-body"]/div[4]/div[2]/div[6]/button',
             '//*[@id="cf-body"]/div[4]/div[2]/div[5]/button',
             '//*[@id="cf-body"]/div[4]/div[2]/div[7]/button',
+            '//*[@id="460-bczs"]/section/div[5]/div/a'
+            
         ]
         
         element_located = False
@@ -81,12 +103,13 @@ class DellBot():
     def tryCoupon(self, couponCode: str, lastPrice:int, **kwargs) -> int:
         currentPrice = lastPrice
         priceReturn = 0
+        
         try:
-            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/input').clear()
-            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/input').send_keys(couponCode)
-            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/span/button').click()
+            self.browser.find_element(By.XPATH, '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/input').clear()
+            self.browser.find_element(By.XPATH, '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/input').send_keys(couponCode)
+            self.browser.find_element(By.XPATH, '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/span/button').click()
             time.sleep(SLEEP_TIME)
-            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/span/button').click()
+            self.browser.find_element(By.XPATH, '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/applycoupon/div[1]/div/div/div/span/button').click()
             time.sleep(SLEEP_TIME)
             currentPrice = int(self.browser.find_element(By.XPATH, '//*[@id="nonpcaas-cart-summary"]/div[3]/ul/li/div[2]/strong').text[2:].replace(',', '').replace('.', '')) 
         except:
@@ -108,26 +131,39 @@ class DellBot():
         return priceReturn
     
     def removeFromCart(self):
-        try:
-            self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[2]/section/div[2]/div/div[2]/div/div/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a').click()
-        except:
+
+        xpath_list = [
+            '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[1]/section/div[2]/div/div[2]/div/div/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a',
+            '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[2]/section/div[2]/div/div[2]/div/div/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a',
+            '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[2]/section/div[2]/div/div[2]/div/div/div[1]/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a',
+            '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[1]/section/div[2]/div/div[2]/div/div/div[1]/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a',
+            '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[1]/section/div[2]/div/div[2]/div/div/div[1]/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a'
+        ]
+        
+        removed = False
+        for xpath in xpath_list:
             try:
-                self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/section/div/div/div/div[2]/section/div[2]/div/div[2]/div/div/div[1]/div[4]/div[1]/div[3]/div[2]/div[1]/div/item-quantity/div/div/a').click()
+                self.browser.find_element(By.XPATH, xpath).click()
+                removed = True
+                break
             except:
-                time.sleep(30)
-                self.removeFromCart()
+                pass
+                
+        if not removed:
+            time.sleep(30)
+            self.removeFromCart()
         time.sleep(SLEEP_TIME)
         
     def removeCoupon(self, couponRank, restarted=False):
         try:
             if couponRank == 1:
-                self.browser.find_element(By.XPATH, f'/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[1]/div[1]/div[2]/p[2]/small/a[2]').click()
+                self.browser.find_element(By.XPATH, f'/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[1]/div/div[2]/p[2]/small/a[2]').click()
                 time.sleep(SLEEP_TIME)
-                self.browser.find_element(By.XPATH, f'/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[3]/div/removeappliedcouponmodal/div[3]/button[2]').click()
+                self.browser.find_element(By.XPATH, f'/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[3]/div/removeappliedcouponmodal/div[3]/button[2]').click()
             else:
-                self.browser.find_element(By.XPATH, f'/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[1]/div[2]/div[2]/p[2]/small/a[2]').click()
+                self.browser.find_element(By.XPATH, f'/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[1]/div[2]/div[2]/p[2]/small/a[2]').click()
                 time.sleep(SLEEP_TIME)
-                self.browser.find_element(By.XPATH, '/html/body/div[4]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[5]/div/removeappliedcouponmodal/div[3]/button[2]').click()
+                self.browser.find_element(By.XPATH, '/html/body/div[3]/div/section/div/div/div[1]/div[1]/div[2]/div[2]/div[5]/aside[1]/div/div/desktopappliedcouponmessagewrapper/div[5]/div/removeappliedcouponmodal/div[3]/button[2]').click()
         except Exception as e:
             print('Reiniciando processo de remover cupom.')
             time.sleep(30)
@@ -170,7 +206,7 @@ class DellBot():
         return cashback
     
 if __name__ == '__main__':
-    url = 'https://www.dell.com/pt-br/shop/notebooks/novo-notebook-gamer-dell-g15/spd/g-series-15-5530-laptop/g5530w010w'
+    url = 'https://www.dell.com/pt-br/shop/mochila-dell-gaming/apd/460-bczs/acess%C3%B3rios-para-jogos?cjevent=b00fbf172b2e11ee83f2ff3f0a82b832&publisherid=6608840&publisher=&aff=Ishii+Producoes&affid=6608840&aff_webid=100921865&aff_user_id=&cjdata=MXxOfDB8WXww&gacd=9657105-29789130-5750457-364944948-190032919&dgc=af&VEN1=100921865&dclid=CMyAzv3gqoADFfdD3QIdGNsPxg'
     k = DellBot()
     price = k.accessCart(url)
     available, price = k.tryCoupon('BEMVINDO150', lastPrice=price)
